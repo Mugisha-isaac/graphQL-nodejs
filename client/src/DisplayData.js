08d7d47793bc2a990a16c697beda67c88c8601c4
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-04-04 15:12:35 +0200

    ft: fetch all movies on client side

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -12,15 +12,42 @@ const QUERY_ALL_USERS = gql`
   }
 `;
 
-const DisplayData = () => {
-  const { data } = useQuery(QUERY_ALL_USERS);
+const QUERY_ALL_MOVIES = gql`
+   query Movies{
+  movies {
+    id,
+    name,
+    isInTheaters,
+    yearOfPublication
+  }
+}
+`;
 
+const DisplayData = () => {
+  const { data, loading, error } = useQuery(QUERY_ALL_MOVIES);
+
+
+  if(loading){
+    return(
+        <h3>Data is loading.......</h3>
+    )
+  }
   if (data) {
     console.log("data ====> ", data);
   }
+
+  if(error){
+    console.log(`Error found ${error}`)
+  }
   return (
-    <div>
-      <h1>List of users</h1>
+    <div className="userCardsContainer">
+      {data && data.movies.map((movie)=> (
+        <div className="userCard">
+            <span>Name: {movie.name}</span>
+            <span>isInTheaters: {movie.isInTheaters}</span>
+            <span>yearOfPublication: {movie.yearOfPublication}</span>
+        </div>
+      ))}
     </div>
   );
 };
