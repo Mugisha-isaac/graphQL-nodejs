bdc1611a9898003f00d6e31aeb83554f2ffd6546
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-04-04 16:06:51 +0200

    ft: add new user mutation

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -1,4 +1,4 @@
-import { useQuery, useLazyQuery, gql } from "@apollo/client";
+import { useQuery, useLazyQuery, useMutation, gql } from "@apollo/client";
 import { useState } from "react";
 
 const QUERY_ALL_USERS = gql`
@@ -16,9 +16,9 @@ const QUERY_ALL_USERS = gql`
 const QUERY_ALL_MOVIES = gql`
    query Movies{
   movies {
-    id,
-    name,
-    isInTheaters,
+    id
+    name
+    isInTheaters
     yearOfPublication
   }
 }
@@ -35,16 +35,32 @@ const GET_MOVIE_BY_NAME = gql`
 }
 `;
 
+const CREATE_USER_MUTATION = gql`
+    mutation CreateUser($input:CreateUserInput!){
+        createUser(input:$input){
+            id,
+            name,
+            username,
+            nationality
+        }
+    }
+`;
+
 const DisplayData = () => {
 
     const [searchedMovie,setSearchedMovie] = useState('');
+    const [newUser,setNewUser] = useState({
+        name: '',
+        age: 0,
+        username: '',
+    })
 
-  const { data, loading, error } = useQuery(QUERY_ALL_MOVIES);
+  const { data, loading, error } = useQuery(QUERY_ALL_USERS);
   const [fetchMovie, {data: searchedMovieData,loading:movieSearchLoading,error: movieSearchedError}] = useLazyQuery(
     GET_MOVIE_BY_NAME
     );
 
-
+  const [createUser] = useMutation(CREATE_USER_MUTATION);
   if(loading){
     return(
         <h3>Data is loading.......</h3>
@@ -55,14 +71,17 @@ const DisplayData = () => {
     console.log(`Error found ${error}`)
   }
 
+  console.log("data is....", data)
+  console.log("error found is.......", error)
+
   return (
     <div className="wrapper">
         <div className="userCardsContainer">
-      {data && data.movies.map((movie)=> (
+      {data && data.users.map((user)=> (
         <div className="userCard">
-            <span>Name: {movie.name}</span>
-            <span>isInTheaters: {movie.isInTheaters}</span>
-            <span>yearOfPublication: {movie.yearOfPublication}</span>
+            <span>Name: {user.name}</span>
+            <span>age: {user.age}</span>
+            <span>username: {user.username}</span>
         </div>
       ))}
 
@@ -79,13 +98,28 @@ const DisplayData = () => {
 
         <div>
             {searchedMovieData && (
-                <div>
-                    <span>Movie name: {searchedMovieData.name}</span>
-                    <span>isInTheaters: {searchedMovieData.isInTheaters}</span>
-                    <span>yearOfPublication: {searchedMovieData.yearOfPublication}</span>
-                </div>
+                <div className="userCard">
+                <span>Name: {searchedMovieData.movie.name}</span>
+                <span>isInTheaters: {searchedMovieData.movie.isInTheaters}</span>
+                <span>yearOfPublication: {searchedMovieData.movie.yearOfPublication}</span>
+            </div>
             )}
         </div>
+      </div>
+
+      <div>
+        <div>
+            <input type="text" placeholder="name" onChange={(e)=>{setNewUser({...newUser, name: e.target.value})}} />
+            <input type="text" placeholder="age" onChange={(e)=>{setNewUser({...newUser, age: parseInt(e.target.value)})}} />
+            <input type="text" placeholder="username" onChange={(e)=>{setNewUser({...newUser, username: e.target.value})}} />
+        </div>
+        <button onClick={()=>{
+            createUser({
+                variables:{
+                    input:newUser
+                }
+            })
+        }}>Create User</button>
       </div>
     </div>
   );
