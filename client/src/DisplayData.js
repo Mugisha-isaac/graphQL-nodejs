d04a21706073b29f25062649685bf8b9f4686d5a
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-04-04 15:35:37 +0200

    ft: add search movie by name client

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -1,4 +1,5 @@
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, useLazyQuery, gql } from "@apollo/client";
+import { useState } from "react";
 
 const QUERY_ALL_USERS = gql`
   query Users {
@@ -23,8 +24,25 @@ const QUERY_ALL_MOVIES = gql`
 }
 `;
 
+const GET_MOVIE_BY_NAME = gql`
+  query Movie($name: String!) {
+  movie(name: $name) {
+     id,
+     name,
+     isInTheaters,
+     yearOfPublication  
+  }
+}
+`;
+
 const DisplayData = () => {
+
+    const [searchedMovie,setSearchedMovie] = useState('');
+
   const { data, loading, error } = useQuery(QUERY_ALL_MOVIES);
+  const [fetchMovie, {data: searchedMovieData,loading:movieSearchLoading,error: movieSearchedError}] = useLazyQuery(
+    GET_MOVIE_BY_NAME
+    );
 
 
   if(loading){
@@ -32,15 +50,14 @@ const DisplayData = () => {
         <h3>Data is loading.......</h3>
     )
   }
-  if (data) {
-    console.log("data ====> ", data);
-  }
 
   if(error){
     console.log(`Error found ${error}`)
   }
+
   return (
-    <div className="userCardsContainer">
+    <div className="wrapper">
+        <div className="userCardsContainer">
       {data && data.movies.map((movie)=> (
         <div className="userCard">
             <span>Name: {movie.name}</span>
@@ -48,6 +65,28 @@ const DisplayData = () => {
             <span>yearOfPublication: {movie.yearOfPublication}</span>
         </div>
       ))}
+
+    </div>
+    <div>
+        <input type="text" placeholder="inception......" onChange={(e)=>{setSearchedMovie(e.target.value)}} />
+        <button onClick={()=>{
+            fetchMovie({
+                variables:{
+                    name: searchedMovie
+                }
+            })
+        }}>Fetch Data</button>
+
+        <div>
+            {searchedMovieData && (
+                <div>
+                    <span>Movie name: {searchedMovieData.name}</span>
+                    <span>isInTheaters: {searchedMovieData.isInTheaters}</span>
+                    <span>yearOfPublication: {searchedMovieData.yearOfPublication}</span>
+                </div>
+            )}
+        </div>
+      </div>
     </div>
   );
 };
